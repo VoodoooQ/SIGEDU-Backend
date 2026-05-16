4eebce44c4b0d94402e89c9e50c07df1326e56f2
Francisco-dev-code <fran.gonzalez.chea@gmail.com>
2026-05-16 09:59:15 -0400

    test: fix de los test

--- calendario/src/test/java/com/gestion/educativa/difusion/calendario/CalendarioApplicationTests.java
+++ calendario/src/test/java/com/gestion/educativa/difusion/calendario/CalendarioApplicationTests.java
@@ -2,8 +2,9 @@ package com.gestion.educativa.difusion.calendario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.gestion.educativa.calendario.calendario.CalendarioApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = CalendarioApplication.class)
 class CalendarioApplicationTests {
 
 	@Test
